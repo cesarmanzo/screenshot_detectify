cefc0187e11d25db993ddef458a2ff971563a574
César Manzo <cesar.manzo@miurabox.com>
2019-01-26 13:06:04 -0600

    Ups, missing () didn't closed the background services

--- v1.py
+++ v1.py
@@ -7,7 +7,7 @@ br.add_argument('headless')
 br.add_argument('hide-scrollbars')
 br.add_argument('kiosk')
 driver = webdriver.Chrome(chrome_options=br)
-driver.get('https://saam.digital')
+driver.get('https://mercadolibre.com.mx')
 height = driver.execute_script(
     """return Math.max(document.body.scrollHeight,
         document.body.offsetHeight,
@@ -20,5 +20,5 @@ driver.set_window_size(1366, height)
 title = driver.title
 time.sleep(2)
 driver.get_screenshot_as_file('XXXXX.png')
-driver.quit
+driver.quit()
 print('END', title)
